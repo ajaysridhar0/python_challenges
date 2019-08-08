e105f9b466f97c4b5bb53a311866abd3c8f25aae
ajaysridhar0 <30612984+ajaysridhar0@users.noreply.github.com>
2019-08-07 20:38:22 -0700

    Add challenges1.py with 7 python challenges

---
+++ challenges1.py
@@ -0,0 +1,64 @@
+
+# Problem 1
+# Input: Integer n
+# Output: Prints n number of fibonacci numbers starting from 0
+
+def fibonacci(n):
+    # write code here
+
+
+
+# Problem 2
+# Input: Integer n
+# Output: The sum of all integers less then n that are divisible by 3 and 5
+
+def threeFiveSum(n):
+    # write code here
+
+print threeFiveSum(1000)
+
+
+
+# Problem 3
+# Input: a list of integers
+# Output: the sum of all even integers in the list
+
+def sumOfAllEvens():
+    # write all code here
+
+print(sumOfAllEvens([2, 43, 523, 232, 24]))
+
+
+
+# Problem 4
+# Code a game of rock, paper, scissors
+
+def playRockPaperScissors():
+    # write code here
+
+# Problem 5
+# Input: Integer n
+# Output: n factorial (n!)
+# Bonus: Use recursion in your function
+
+def factorial(n):
+    # write code here
+
+# Problem 6
+# Input: String
+# Output: The reverse of the string
+# Example: reverse("potato") would return the string "otatop"
+# Bonus: Use recursion in your function
+
+def reverseString(s):
+    # write code here
+
+print(reverseString("tomato"))
+
+# Problem 7
+# Input: 2 strings
+# Output: the number of the positions where the two strings contain the same length 2 substring.
+# So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+
+def def stringMatch(a, b):
+    # write code
